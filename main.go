e2212a01204cf7c273ffd0cf8cd3e254e790da3c
Reskyppo <reskybeen@gmail.com>
2021-09-07 21:37:14 +0700

    add go mod and write code connect to db

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
+var db *gorm.DB
+var err error
+
 func main() {
-	log.Println("This is initial project")
+	db, err = gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/Basketball?charset=utf8&parseTime=True")
+
+	if err != nil {
+		log.Println("Connection Failed to Open because", err)
+	} else {
+		log.Println("Connection Established")
+	}
+
 }
