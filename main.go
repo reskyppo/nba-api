473ad33c53cfb6e9b034b38bbb7f84fd8d97916c
Reskyppo <reskybeen@gmail.com>
2021-09-08 10:53:45 +0700

    [add] config index route

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 )
 
@@ -18,6 +21,23 @@ type Teams struct {
 	Division string `json:"division"`
 }
 
+// Func to handle content from home page
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "This is HomePage!")
+	fmt.Println("Endpoint for homepage")
+}
+
+// Func to handle request and create router
+func handleRequests() {
+	log.Println("Starting development server at http://127.0.0.1:8008/")
+	log.Println("Quit the server with CONTROL-C.")
+
+	// create a router using mux
+	myRouter := mux.NewRouter().StrictSlash(true)
+	//endpoint "/" and the content is from func homePage
+	myRouter.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8008", myRouter)) //set the port and handler
+}
 func main() {
 	// Define DB name, DB username, DB password
 	db, err = gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/Basketball?charset=utf8&parseTime=True")
@@ -28,7 +48,11 @@ func main() {
 	} else {
 		log.Println("Connection Established")
 	}
-	
-	// Create Table Teams 
+
+	// Create Table Teams
 	db.AutoMigrate(&Teams{})
+
+	//initiate func for request
+	handleRequests()
+
 }
